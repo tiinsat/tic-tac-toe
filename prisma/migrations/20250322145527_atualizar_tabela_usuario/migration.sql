c4a293be8bfaa254e8fcf1a90e66c7612129c590
Ivan Pires da Silva Ferreira <ivan.pires2000@gmail.com>
2025-03-22 15:16:23 +0000

    feat: criar formulario de cadastro

---
+++ prisma/migrations/20250322145527_atualizar_tabela_usuario/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `avatar` to the `usuario` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `usuario` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `usuario` ADD COLUMN `avatar` VARCHAR(191) NOT NULL,
+    ADD COLUMN `username` VARCHAR(191) NOT NULL;
--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
 const app = express();
+const prisma = new PrismaClient();
+
 const PORT = 8080;
+
 
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
     res.json({ message: "API funcionando corretamente!" });
 });
+
+app.post("/api/v1/usuarios", async (req, res) =>{
+    const   {
+                nome, 
+                email, 
+                senha, 
+                username, 
+                avatar
+            } = req.body;
+
+    const usuario = await prisma.usuario.create({
+        data: {
+            nome, 
+            email, 
+            senha, 
+            username, 
+            avatar
+        }        
+    });
+
+    res.status(201).json(usuario);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
