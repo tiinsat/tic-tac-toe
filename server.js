1f1d645b4eb1b9d765d66954285413c24e53265c
ivanpires2000 <ivan.pires2000@gmail.com>
2025-04-05 14:55:40 +0000

    feat: acriar jogador

--- server.js
+++ server.js
@@ -11,66 +11,92 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
-    res.json({ message: "API funcionando corretamente!" });
+  res.json({ message: "API funcionando corretamente!" });
 });
 
-app.post("/api/v1/usuarios", async (req, res) =>{
-    const   {
-                nome, 
-                email, 
-                senha, 
-                username, 
-                avatar
-            } = req.body;
+app.post("/api/v1/usuarios", async (req, res) => {
+  const { nome, email, senha, username, avatar } = req.body;
 
-    const usuario = await prisma.usuario.create({
-        data: {
-            nome, 
-            email, 
-            senha, 
-            username, 
-            avatar
-        }        
-    });
+  const usuario = await prisma.usuario.create({
+    data: {
+      nome,
+      email,
+      senha,
+      username,
+      avatar,
+    },
+  });
 
-    res.status(201).json(usuario);
+  res.status(201).json(usuario);
 });
 
 // app.post("/api/v1/conexao", async(data, response))
-app.post("/api/v1/conexao", async(req, res)=>{
-    // console.log(req, res);
-    const {
-        id_usuario_a,
-        id_usuario_b
-    } = req.body;
+app.post("/api/v1/conexao", async (req, res) => {
+  // console.log(req, res);
+  const { id_usuario_a, id_usuario_b } = req.body;
 
-    console.log(prisma.conexao)
+  console.log(prisma.conexao);
 
-    // const conexao = await prisma.conexao.create({
-    //     data: {
-    //         id_usuario_a,
-    //         id_usuario_b
-    //     }
-    // });
+  // const conexao = await prisma.conexao.create({
+  //     data: {
+  //         id_usuario_a,
+  //         id_usuario_b
+  //     }
+  // });
 
-    // console.log(conexao);
-    // res.status(201).json(conexao);
-})
+  // console.log(conexao);
+  // res.status(201).json(conexao);
+});
 
+app.get("/api/v1/conexao", async (req, res) => {
+  const conexao = await prisma.conexao.findMany();
+  res.status(200).json(conexao);
+});
 
-app.post('/api/v1/login', async(req, res) => {
-    const {email, senha }= req.body;
-    const usuario = await prisma.usuario.findUnique({where:{email, senha}});
+app.post("/api/v1/login", async (req, res) => {
+  const { email, senha } = req.body;
+  const usuario = await prisma.usuario.findUnique({ where: { email, senha } });
 
-    if(usuario == null){
-        return res.status(401).json({error:"Credenciais Inválidas"});
-    }
-    else{
-        return res.status(200).json(usuario);
-    }
-})
-
+  if (usuario == null) {
+    return res.status(401).json({ error: "Credenciais Inválidas" });
+  } else {
+    return res.status(200).json(usuario);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.post("/api/v1/partidaHistorico", async (req, res) => {
+  const { id_partida, id_usuario, area_jogada, peca_do_jogador } = req.body;
+
+  const ultimaJogada = await prisma.partida_historico.findFirst({
+    where: { id_partida },
+    orderBy: { id: "desc" },
+  });
+
+  const localOcupado = await prisma.partida_historico.findFirst({
+    where: {
+      area_jogada,
+      id_partida,
+    },
+  });
+
+  if (ultimaJogada["id_usuario"] == id_usuario) {
+    res.status(422).json("Não é sua vez de jogar");
+  } else if (localOcupado != null) {
+    res.status(422).json("Posição ja ocupada");
+  } else {
+    const partida_historico = await prisma.partida_historico.create({
+      data: {
+        id_partida,
+        id_usuario,
+        area_jogada,
+        peca_do_jogador,
+      },
+    });
+
+    res.status(201).json(partida_historico);
+  }
 });
