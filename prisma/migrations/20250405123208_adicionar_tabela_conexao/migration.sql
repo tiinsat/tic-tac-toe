f0baa7339b98db3b124dc066ee0bc66344885f3a
tiinsat <103745487+tiinsat@users.noreply.github.com>
2025-05-01 18:54:41 +0000

    ATUALIZAÇÃO

---
+++ prisma/migrations/20250405123208_adicionar_tabela_conexao/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `conexao` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_usuario_a` INTEGER NOT NULL,
+    `id_usuario_b` INTEGER NOT NULL,
+    `data_solicitacao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status` ENUM('criado', 'andamento', 'finalizado') NOT NULL DEFAULT 'criado',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20250405142249_partida_historico/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `partida_historico` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_partida` INTEGER NOT NULL,
+    `id_usuario` INTEGER NOT NULL,
+    `area_jogada` INTEGER NOT NULL,
+    `peca_do_jogador` ENUM('x', 'o') NOT NULL,
+    `data_solicitacao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
